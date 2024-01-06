cb6198b8088553dc4ecb6a082a0dd6654de0a1f3
Lazy_Boy <li0nleo117@gmail.com>
2024-01-06 13:53:53 +0000

    comments

--- src/ParticleSystem.h
+++ src/ParticleSystem.h
@@ -4,8 +4,9 @@
 #include "Camera.h"
 class Particle {
 public:
+	//initalize the particle positions, velocity and acceleration
 	void Init(float lifeTime, int posx, int posy, double velX, double velY, double xInc,double yInc);
-	void Update(float tpf);
+	void Update(float tpf);//update particle
 	float lifetime=0;//0 = dead
 
 	//position
@@ -20,17 +21,17 @@ public:
 class ParticleSystem
 {
 public:
-	ParticleSystem(int _ParticleAmount,float _spawnRate,float _lifeTime);
+	ParticleSystem(int _ParticleAmount,float _spawnRate,float _lifeTime); //initalize particle system
 	~ParticleSystem();
-	void Reset();
-	void Update(float tpf, int x, int y,int vX, int vY, bool CreateNewParticles);
-	void Render(SDL_Renderer* renderer, SDL_Colour color);
+	void Reset(); //reset particles
+	void Update(float tpf, int x, int y,int vX, int vY, bool CreateNewParticles); //update particles
+	void Render(SDL_Renderer* renderer, SDL_Colour color); // render particles
 private:
-	int RandomRange(int min, int max);
-	Particle* Particles;
-	int ParticleAmount;
-	float spawnRate;
-	float TimeTillSpawn=0;
-	float lifeTime;
+	int RandomRange(int min, int max); //used to randomise particle velocity and direction
+	Particle* Particles; //pointer to array of particle objects
+	int ParticleAmount; //amount of particles allocated
+	float spawnRate; //rate in mili seconds that the particles spawn
+	float TimeTillSpawn=0; //remaining time to spawn next particle
+	float lifeTime; //life time for each particle before being recycled
 };
 
